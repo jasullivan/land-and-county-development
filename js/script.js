37c9c38ec7c2b57266f1253868a998ac2d6d88c2
James Sullivan <james.sullivan@live.co.uk>
2019-10-26 08:10:32 +0100

    simplifying scss and amending mob nav

--- js/script.js
+++ js/script.js
@@ -1,19 +1,41 @@
-window.onscroll = function () { myFunction() };
+// sticky nav
+window.onscroll = function () { stickyNav() };
 
 var navbar = document.querySelector(".navigation");
 var sticky = navbar.offsetTop + 100;
 
-function myFunction() {
+function stickyNav() {
     if (window.pageYOffset >= sticky) {
         navbar.classList.add("sticky")
     } else {
         navbar.classList.remove("sticky");
     }
 }
-function openNav() {
-    document.getElementById("mySidenav").style.height = "100vh";
-}
+// sticky nav ends
 
-function closeNav() {
-    document.getElementById("mySidenav").style.height = "0";
-}
+// function openNav() {
+//     document.getElementById("mySidenav").style.height = "100vh";
+// }
+
+// function closeNav() {
+//     document.getElementById("mySidenav").style.height = "0";
+// }
+
+// mob nav button
+const button = document.querySelector('.logo-box__menu-button');
+const menu = document.querySelector('.navigation');
+button.addEventListener('click', e => {
+    menu.classList.toggle('show-menu');
+})
+// mob nav button ends
+
+// on resize hide mob nav
+document.addEventListener("DOMContentLoaded", function(e) {
+    function resize() {
+        if (window.innerWidth > 1200) {
+        menu.classList.remove('show-menu');
+        }
+    }
+  window.onresize = resize;
+});
+// on resize hide mob nav ends
